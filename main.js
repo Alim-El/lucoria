a28eb057d6d84d664848e18dc59a72e8330f994b
Alim-kbr <alim.alimov1@gmail.com>
2019-01-23 17:13:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -76,13 +76,45 @@ $('a[href*="#"]').click(function() {
     return false;
 });
 
-
-$('.header__menu-inner li a').each(function() {
-  if ('https://alim-kbr.github.io/lucoria/'+$(this).attr('href') === window.location.href)
-  {
-      $(this).addClass('active');
+var menu_selector = ".header__menu";
+function onScroll() {
+  var scroll_top = $(document).scrollTop();
+  $(menu_selector + " a").each(function(){
+    var hash = $(this).attr("href");
+    var target = $(hash);
+    if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+    $(menu_selector + " a.active").removeClass("active");
+    $(this).addClass("active");
+    } else {
+    $(this).removeClass("active");
   }
 });
+}
+
+$(document).on("scroll", onScroll);
+$("a[href^='#']").click(function(e){
+  e.preventDefault();
+  $(document).off("scroll");
+  $(menu_selector + " a.active").removeClass("active");
+  $(this).addClass("active");
+  var hash = $(this).attr("href");
+  var target = $(hash);
+$("html, body").animate({
+  scrollTop: target.offset().top
+}, 500, function(){
+  window.location.hash = hash;
+  $(document).on("scroll", onScroll);
+  });
+});
+
+
+
+// $('.header__menu-inner li').each(function() {
+//   if ('https://alim-kbr.github.io/lucoria/'+$(this).attr('href') === window.location.href)
+//   {
+//       $(this).addClass('active');
+//   }
+// });
 
 
 
